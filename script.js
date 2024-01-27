cfd3b12869e2bd190f985e558cc517e1468a8137
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-01-27 10:31:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const body = document.querySelector('body');
 
-body.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded',()=>{
 	body.innerText = 'DOM load success';
 });
